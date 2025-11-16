fec31d60598025e67a0683da145a7e6a5e81f43e
Nicolas <castel.nico21@gmail.com>
2025-11-16 11:10:19 +0100

    Debug

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -3,12 +3,11 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { MenuComponent } from "./pages/menu/menu.component";
-import { HeaderComponent } from "./pages/header/header.component";
 import { NetworkComponent } from "./pages/network/network.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NzIconModule, NzLayoutModule, NzMenuModule, MenuComponent, HeaderComponent, NetworkComponent],
+  imports: [NzIconModule, NzLayoutModule, NzMenuModule, MenuComponent, NetworkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
